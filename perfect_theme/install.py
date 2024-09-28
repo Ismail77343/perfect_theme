d72f3b8f2813957ff8f9acfe0f94d3468528fc6a
erpalfras <erpalfras@erpalfras.myguest.virtualbox.org>
2024-09-28 11:28:47 +0300

    change to salery

--- perfect_theme/install.py
+++ perfect_theme/install.py
@@ -1,6 +1,7 @@
 import frappe;
 from frappe import _
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
 
 
